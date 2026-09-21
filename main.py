f01b46d6dda067f3428ef46530be3a3d40e1141e
DiamondArrowTheLizard <diamond1arrow@gmail.com>
2026-09-21 17:09:06 +0700

    add export to csv

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import app as a
 import crosstabs as ct
+import export_csv as ec
 import load_data as ld
 import show_correlation as cor
 
 if __name__ == "__main__":
     X, y, crosstab_list, correlation_list = ld.load_data()
+    
+    out_dir = ec.export_all(X, y, crosstab_list, correlation_list)
+    print(f"Exported data to ./{out_dir}/")
 
     app = a.AutomobileApp(X, y, crosstab_list, correlation_list)
     app.mainloop()
